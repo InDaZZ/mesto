8df020428972de369a72880e7abf42a33177f006
Evgeniy Dekhtyarev <f-k-87@list.ru>
2022-11-23 19:01:31 +0300

    ревью

--- scripts.js/index.js
+++ scripts.js/index.js
@@ -1,4 +1,4 @@
-let addBotton = document.querySelector('.profile__add-button-container');
+let editButton = document.querySelector('.profile__edit-button');
 let bottonClear = document.querySelector('.popup__botton-clear');
 let popup = document.querySelector('.popup');
 let name = document.querySelector('.profile__name');
@@ -10,24 +10,32 @@ let formElement = document.querySelector('.form');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    popupBotton.addEventListener ('click', function() {
-        name.textContent = popupName.value;
-        activity.textContent = popupActivity.value;
-    })
+    
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 
+function saveChanges() {
+    name.textContent = popupName.value;
+    activity.textContent = popupActivity.value;
+    popup.classList.remove('popup_active')
+};
 
-addBotton.addEventListener ('click', function() {
+popupBotton.addEventListener ('click', saveChanges);
+
+function popupActive() {
     popup.classList.add('popup_active')
     popupName.value = name.textContent;
     popupActivity.value = activity.textContent;
-});
+}
 
-bottonClear.addEventListener ('click', function() {
+editButton.addEventListener ('click', popupActive);
+
+function popupClose() {
     popup.classList.remove('popup_active')
-});
+}
+
+bottonClear.addEventListener ('click', popupClose);
 
 
 
