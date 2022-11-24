0fda4d695bd43048cc6923c718470e2fa05bf3e5
Evgeniy Dekhtyarev <f-k-87@list.ru>
2022-11-24 14:36:16 +0300

    ревью

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__botton-clear');
+let popup = document.querySelector('.popup');
+let profileNameText = document.querySelector('.profile__name');
+let activity = document.querySelector('.profile__activity');
+let popupBotton = document.querySelector('.popup__botton');
+let popupName = document.querySelector('.popup__item_name');
+let popupActivity = document.querySelector('.popup__item_activity');
+let formElement = document.querySelector('.form');
+
+function popupActive() {
+    popup.classList.add('popup_active')
+    popupName.value = profileNameText.textContent;
+    popupActivity.value = activity.textContent;
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    popupName.getAttribute('value');
+    popupActivity.getAttribute('value');
+    profileNameText.textContent = popupName.value;
+    activity.textContent = popupActivity.value;
+    popup.classList.remove('popup_active')
+}
+
+function popupClose() {
+    popup.classList.remove('popup_active')
+}
+
+editButton.addEventListener ('click', popupActive);
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+popupCloseButton.addEventListener ('click', popupClose);
+
+
+
+
+
+
+
+
+ 
+
